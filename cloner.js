5e35f384676f7dca28a841b2fe312ac9155eb8ff
ridjic.darko <ridjic.darko@nsoft.com>
2020-03-02 17:52:48 +0100

    add

--- cloner.js
+++ cloner.js
@@ -1,12 +1,17 @@
 var cloner = {
   init: function() {
     var body = document.getElementsByTagName('body')[0];
-    var element = document.getElementsByClassName('some-text')[0];
+    var element = document.getElementsByClassName('post')[0];
 
     for (var i = 0; i < 5; i++) {
       var cloned = element.cloneNode(true);
 
       body.appendChild(cloned);
+    }
+
+    var likes = document.getElementsByClassName('like-number');
+    for (var i = 0; i < likes.length; i++) {
+      likes[i].textContent = Math.floor(Math.random() * 500);
     }
   }
 };
